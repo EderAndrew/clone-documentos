8471710d9c86f7d24515a4a7667b11986d4508d7
Eder Andrew <ederandrew0028@gmail.com>
2023-10-16 15:11:11 -0300

    Corrigindo server

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: false,
+    typescript: {
+        ignoreBuildErrors: true,
+     },
+    experimental:{
+        serverActions: true
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.node/,
+            use: 'raw-loader',
+            
+        });
+        
+       return config;
+    },
+}
 
 module.exports = nextConfig

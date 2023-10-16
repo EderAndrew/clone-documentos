ef7b1147f47a718a56b15bc4d6002bc29dcf95b1
Eder Andrew <ederandrew0028@gmail.com>
2023-10-16 15:29:26 -0300

    Criando conexão com json-server

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useContext } from 'react'
+import { SessionContext } from "@/providers/sessionProvider"
+
+export const Loading = () => {
+    const ctx = useContext(SessionContext)
+    return (
+        <>
+            {ctx?.loading && (
+                <div className="fixed z-20 w-screen h-screen bg-slate-900/60 flex justify-center items-center">
+                    <span className="text-white text-2xl">Carregando...</span>
+                </div>
+            )}
+        </>
+    )
+}
